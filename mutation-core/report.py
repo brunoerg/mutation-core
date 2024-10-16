38af9126efc55e887044ed54a560fb6b4b037e3a
Bruno Garcia <brunoely.gc@gmail.com>
2024-10-16 11:11:55 -0300

    fix report.py (lint)

--- mutation-core/report.py
+++ mutation-core/report.py
@@ -31,7 +31,9 @@ def report(not_killed_mutants=[], folder="", original_file="", score=0):
             modified_file = os.path.join(folder, filename)
 
             # Use git diff --no-index to compare the files and capture the output
-            result = subprocess.run(['git', 'diff', '--no-index', original_file, modified_file], capture_output=True, text=True)
+            result = subprocess.run(['git', 'diff', '--no-index',
+                                     original_file, modified_file],
+                                     capture_output=True, text=True)
             print(result.stdout)
             print("--------------")
 
@@ -39,7 +41,7 @@ def report(not_killed_mutants=[], folder="", original_file="", score=0):
             report_data["diffs"].append(result.stdout)
 
     # Export the report as a JSON file
-    json_file = f'diff_not_killed.json'
+    json_file = 'diff_not_killed.json'
     with open(json_file, 'w') as file:
         json.dump(report_data, file, indent=4)
 
