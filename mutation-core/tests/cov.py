d50ae1a749d39423a26d9df0d966bef313fea56c
Bruno Garcia <brunoely.gc@gmail.com>
2024-10-21 12:43:33 -0300

    add tests for coverage file

---
+++ mutation-core/tests/cov.py
@@ -0,0 +1,21 @@
+import unittest
+import os
+
+from ..cov import parse_coverage_file
+
+
+class TestCoverage(unittest.TestCase):
+    def test_parse_coverage_file(self):
+        res = parse_coverage_file(f'{os.path.dirname(os.path.abspath(__file__))}/total_coverage.info')
+        self.assertEqual(res['/Users/fakeuser/projects/bitcoin-core/src/addrdb.cpp'],
+                         [31, 95, 97, 99, 100, 102, 107, 110, 117, 131, 132, 133, 134,
+                          135, 137, 139, 140, 141, 148, 150, 152, 156, 157, 160, 161,
+                          163, 171, 172, 177, 178, 186, 188, 189, 305])
+
+    def test_parse_wrong_coverage_file(self):
+        with self.assertRaises(FileNotFoundError):
+            parse_coverage_file('total_coverage.info')
+
+
+if __name__ == '__main__':
+    unittest.main()
