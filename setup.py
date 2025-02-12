fd821811a5b5df83b1357ab9d747049e01f6ff24
Bruno Garcia <brunoely.gc@gmail.com>
2025-02-12 10:05:01 -0300

    bump setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mutation-core',
-    version='0.1.5',
+    version='0.1.6',
     install_requires=[],
     packages=find_packages(),
     entry_points={
